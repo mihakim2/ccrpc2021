c725bde8f780e623b41ae327993d331cfa6360f5
mihakim2 <mihakim2@illinois.edu>
2021-10-17 10:29:27 -0500

    Uploaded project

--- ccrpc_collect/admin.py
+++
@@ -1,7 +0,0 @@
-from leaflet.admin import LeafletGeoAdmin
-from django.contrib import admin
-
-from . import models as ccrpc_models
-
-
-admin.site.register(ccrpc_models.DataPointSpot, LeafletGeoAdmin)
